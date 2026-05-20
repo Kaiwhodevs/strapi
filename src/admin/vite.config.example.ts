b9b233fde3c1b2f1e52c7b4f750af781ee6c2f82
Kai <github.snarl729@passmail.com>
2026-05-21 00:15:43 +0700

    Add server allowedHosts configuration to Vite config

--- src/admin/vite.config.example.ts
+++ src/admin/vite.config.example.ts
@@ -1,5 +1,4 @@
 import { mergeConfig, type UserConfig } from 'vite';
-
 export default (config: UserConfig) => {
   // Important: always return the modified config
   return mergeConfig(config, {
@@ -7,6 +6,9 @@ export default (config: UserConfig) => {
       alias: {
         '@': '/src',
       },
+    },
+    server: {
+      allowedHosts: true,
     },
   });
 };
